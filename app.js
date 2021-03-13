24d6d678ff5fd83d37184ffd1ecef0b68accd77d
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-03-13 18:04:51 +0330

    connect to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
@@ -16,3 +17,15 @@ app.listen(port, () => {
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+// Connect to db
+const db = process.env.LOCAL_DATABASE;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: false,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    config: { autoIndex: false },
+  })
+  .then(() => console.log('Database was successfuly connected....!'));
